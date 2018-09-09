545b173a5cfd7db421b5cf343d56f94b5c13d7e0
muhammadkamil66 <kamilhanifsoomro@gmail.com>
2018-09-09 11:47:14 -0700

    fix issues

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $.ajax({
-	url: "https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=4c1f191459014feaac61cf27485157f9",
+	url: "https://newsapi.org/v2/top-headlines?sources=polygon&apiKey=4c1f191459014feaac61cf27485157f9",
 	success: function(data){
 
-		for(let i = 1; i <= 6; i++){
+		for(let i = 1; i <= 9; i++){
 			document.querySelector(".main").innerHTML+=`
 			<div class="news">
 				<h1 class="author">${data.articles[i].author}</h1>
 			<div>
 			<div class="img" style="background-image: url(${data.articles[i].urlToImage})"></div>
 				<p class="title">${data.articles[i].title}</p>
-				<a href="${data.articles[i].url}" target="_blank"><button class="btn btn-outline-dark">Read More</button></a>
+				<a href="${data.articles[i].url}" target="_blank"><button class="btn btn-outline-dark btn-sm">Read More</button></a>
 			</div>
 			</div>
 			`
@@ -26,14 +26,14 @@ $.ajax({
 $.ajax({
 	url: "https://newsapi.org/v2/top-headlines?sources=the-next-web&apiKey=4c1f191459014feaac61cf27485157f9",
 	success: function(data){
-		for(let i = 1; i <= 6; i++){			
+		for(let i = 1; i <= 9; i++){			
 			document.querySelector(".main").innerHTML+=`
 			<div class="news">
 				<h1 class="author">${data.articles[i].author}</h1>
 			<div>
 			<div class="img" style="background-image: url(${data.articles[i].urlToImage})"></div>
 				<p class="title">${data.articles[i].title}</p>
-				<a href="${data.articles[i].url}" target="_blank"><button class="btn btn-outline-dark">Read More</button></a>
+				<a href="${data.articles[i].url}" target="_blank"><button class="btn btn-outline-dark btn-sm">Read More</button></a>
 			</div>
 			</div>
 			`
